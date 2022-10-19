4118963894192d3dc99b138ef4253bb3dd423261
Drakodan <traxxdata@laposte.net>
2022-10-19 14:19:53 +0200

    CRUD track

--- src/api/tracks/getAll.js
+++ src/api/tracks/getAll.js
@@ -1,3 +1,15 @@
+const database = require('../database.js');
+
 module.exports = (req, res) => {
-  // your code here !
+  database
+    .query('select * from track')
+    .then(([tracks]) => {
+      res.json(tracks);
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send('Error retrieving data from database');
+    });
 };
