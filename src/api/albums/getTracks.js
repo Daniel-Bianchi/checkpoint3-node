aa24e1c1d990a06b836ea082193963e966ce63fc
Drakodan <traxxdata@laposte.net>
2022-10-19 15:19:58 +0200

    get tracks of an album

--- src/api/albums/getTracks.js
+++ src/api/albums/getTracks.js
@@ -1,3 +1,20 @@
+const database = require('../database.js');
+
 module.exports = (req, res) => {
-  // your code here !
+  const id = parseInt(req.params.id);
+  database
+    .query('select * from track where id_album = ?', [id])
+    .then(([tracks]) => {
+      if (tracks[0] != null) {
+        res.json(tracks);
+      } else {
+        res.status(404).send('Not Found');
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send('Error retrieving data from database');
+    });
 };
