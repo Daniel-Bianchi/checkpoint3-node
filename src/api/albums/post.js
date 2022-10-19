d8550a2fdfa9b5df07867f08732e68bb6a6784ab
Drakodan <traxxdata@laposte.net>
2022-10-19 14:08:24 +0200

    CRUD album

--- src/api/albums/post.js
+++ src/api/albums/post.js
@@ -1,3 +1,20 @@
+const database = require('../database.js');
+
 module.exports = (req, res) => {
-  // your code here !
+  const { title, genre, picture, artiste } = req.body;
+
+  database
+    .query(
+      'INSERT INTO album(title, genre, picture, artiste) VALUES (?, ?, ?, ?)',
+      [title, genre, picture, artiste]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send('Error saving the movie');
+    });
 };
--- src/api/albums/update.js
+++ src/api/albums/update.js
@@ -1,3 +1,25 @@
+const database = require('../database.js');
+
 module.exports = (req, res) => {
-  // your code here !
+  const id = parseInt(req.params.id);
+  const { title, genre, picture, artiste } = req.body;
+
+  database
+    .query(
+      'update album set title = ?, genre = ?, picture = ?, artiste = ? where id = ?',
+      [title, genre, picture, artiste, id]
+    )
+
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not Found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error editing the album');
+    });
 };
